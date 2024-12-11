2479c377e400c11f57064d43d65362435ccf482f
Nadeem Hamdy <70660389+NadeemHamdy@users.noreply.github.com>
2024-12-11 21:25:36 +0200

    Update false.m

--- false.m
+++ false.m
@@ -1,18 +1,15 @@
 function root = false(f, xl, xu, tol)
-    % التحقق من صحة القيم الابتدائية
     if f(xl) * f(xu) >= 0
         error('f(xl) and f(xu) must have opposite signs.');
     end
       root=0;
-    % تعريف الخطأ الابتدائي
-    error = 10000; % تعيين قيمة كبيرة كبداية
+    error = 10000; 
 
     % تطبيق طريقة False Position
     while error > tol
         xr = xl - (f(xl) * (xu - xl)) / (f(xu) - f(xl));
-        error = abs((xr - root) / xr) * 100; % حساب الخطأ النسبي
+        error = abs((xr - root) / xr) * 100; 
           root=xr;
-        % تحديث القيم حسب الإشارة
         if f(xr) == 0
             break;
         elseif f(xl) * f(xr) < 0
@@ -22,9 +19,7 @@ function root = false(f, xl, xu, tol)
         end
     end
 
-    % طباعة الخطأ النسبي النهائي
     fprintf('Final Error = %.4f%%\n', error);
 
-    % النتيجة النهائية
     root = xr;
 end
